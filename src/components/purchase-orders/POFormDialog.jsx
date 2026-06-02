924d78b39ae1cc141ae750d058f08d717067dd73
Michael Rey Zordilla <mzordilla.fcl@gmail.com>
2026-06-02 02:32:53 +0000

    File changes

--- src/components/purchase-orders/POFormDialog.jsx
+++ src/components/purchase-orders/POFormDialog.jsx
@@ -116,7 +116,7 @@ export default function POFormDialog({ open, onOpenChange, title, initialData, o
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-lg max-h-[90vh] overflow-y-auto">
+      <DialogContent className="sm:max-w-3xl max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
